c4051f5a5e1934b4fdb91c15822de021f72ef679
rin <strawberinmilk@gmail.com>
2024-10-14 12:11:16 +0900

    BE初期化とdocker

---
+++ backend.Dockerfile
@@ -0,0 +1,14 @@
+FROM node:20.10-alpine
+
+ENV LANG=ja_JP.UTF-8
+ENV TZ=Asia/Tokyo
+WORKDIR /app
+
+RUN apk update
+RUN apk add yarn
+
+COPY ./backend/* ./
+COPY ./backend/package.json ./
+
+RUN yarn install
+RUN yarn global add @nestjs/cli
--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,27 +1,64 @@
-import { useEffect, useState } from "react";
-import "./App.css";
+import { useState } from "react";
+import { Button } from "@mui/material";
 
 const App = () => {
-  const [latitude, setLatitude] = useState(0); // 緯度
-  const [longitude, setLongitude] = useState(0); // 経度
+  const parkingList = [
+    {
+      'name': '大黒PA',
+      lat: 35.46164868963681,
+      lng: 139.67996120452884,
+      radius: 200,
+    }
+  ]
 
-  function successCallback(position: GeolocationPosition) {
+  const [lat, setLatitude] = useState(0); // 緯度
+  const [lng, setLongitude] = useState(0); // 経度
+
+  // 二地点間の距離の取得
+  const R = Math.PI / 180;
+  const distance = (lat1: number, lng1: number, lat2: number, lng2: number) => {
+    lat1 *= R;
+    lng1 *= R;
+    lat2 *= R;
+    lng2 *= R;
+    const km =
+      6371 *
+      Math.acos(
+        Math.cos(lat1) * Math.cos(lat2) * Math.cos(lng2 - lng1) +
+          Math.sin(lat1) * Math.sin(lat2)
+      );
+    return km * 1000;
+  };
+
+  // 位置情報の取得
+  const getLocation = () => {
+    navigator.geolocation.getCurrentPosition(
+      getLocationSuccess,
+      getLocationError
+    );
+  };
+  const getLocationSuccess = (position: GeolocationPosition) => {
     setLatitude(position.coords.latitude);
     setLongitude(position.coords.longitude);
-  }
-  function errorCallback(error: GeolocationPositionError) {
+  };
+  const getLocationError = (/* error: GeolocationPositionError */) => {
     alert("位置情報が取得できませんでした");
-  }
-  useEffect(() => {
-    navigator.geolocation.getCurrentPosition(successCallback, errorCallback);
-  }, []);
+  };
 
   return (
     <>
-      <p>緯度: {latitude}</p>
-      <p>経度: {longitude}</p>
+      <Button onClick={getLocation}>位置取得</Button>
+      <p>緯度: {lat}</p>
+      <p>経度: {lng}</p>
+      <p>大黒との距離: {distance(lat, lng, parkingList[0].lat, parkingList[0].lng)}</p>
+      {distance(lat, lng, parkingList[0].lat, parkingList[0].lng) < parkingList[0].radius ? (
+        <p>ここは大黒です</p>
+      ) : (
+        <p>ここは大黒ではありません</p>
+      )}
+      <p></p>
     </>
   );
 };
 
-export default App
+export default App;
